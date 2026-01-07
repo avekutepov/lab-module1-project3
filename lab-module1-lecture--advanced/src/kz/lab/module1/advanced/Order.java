dcb3f098a262880f6e856f1721a29b9f665b37c3
Pavel <kutepov_pasha@list.ru>
2026-01-07 16:55:49 +0500

    Add value verifications

--- lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
+++ lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
@@ -1,7 +1,14 @@
 package kz.lab.module1.advanced;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 public record Order(long id, double price, LocalDateTime orderDate, Set<String> items ) {
+    public Order {
+        if(id < 0){throw new IllegalArgumentException("Id can't be negative");}
+        if(price <= 0){throw new IllegalArgumentException("Price should be greater than 0");}
+        if(orderDate.isAfter(LocalDateTime.now())){throw new IllegalArgumentException("Wrong date");}
+        items = Set.copyOf(items);
+    }
 }
