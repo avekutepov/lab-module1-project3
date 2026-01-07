a24da7abcc7926cd6c3269bb6a12d6ae5880c2c7
Pavel <kutepov_pasha@list.ru>
2026-01-07 16:56:15 +0500

    Add value verifications

--- lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
+++ lab-module1-lecture--advanced/src/kz/lab/module1/advanced/Order.java
@@ -1,7 +1,6 @@
 package kz.lab.module1.advanced;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 public record Order(long id, double price, LocalDateTime orderDate, Set<String> items ) {
